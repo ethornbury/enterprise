a4312bbc2c713b77a22669ac8310a31a2042896b
ethornbury <ethornbury@eircom.net>
2015-05-10 16:31:09 +0000

    show dynamic db info in form field

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,20 +1,20 @@
 class EmployeesController < ApplicationController
   before_action :set_employee, only: [:show, :edit, :update, :destroy]
 
-  respond_to :html
+  #respond_to :html
 
   def index
     @employees = Employee.all
-    respond_with(@employees)
+    #respond_with(@employees)
   end
 
   def show
-    respond_with(@employee)
+    #respond_with(@employee)
   end
 
   def new
     @employee = Employee.new
-    respond_with(@employee)
+    #respond_with(@employee)
   end
 
   def edit
@@ -22,18 +22,43 @@ class EmployeesController < ApplicationController
 
   def create
     @employee = Employee.new(employee_params)
-    @employee.save
-    respond_with(@employee)
+    #@employee.save
+    #respond_with(@employee)
+    respond_to do |format|
+      if @employee.save
+        format.html { redirect_to @employee, notice: 'Employee was successfully created.' }
+        format.json { render action: 'show', status: :created, location: @employee }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @employee.errors, status: :unprocessable_entity }
+      end
+    end
+    
+    
   end
 
   def update
     @employee.update(employee_params)
-    respond_with(@employee)
+    #respond_with(@employee)
+    respond_to do |format|
+      if @employee.update(employee_params)
+        format.html { redirect_to @employee, notice: 'Employee was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: @employee.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
     @employee.destroy
-    respond_with(@employee)
+    #respond_with(@employee)
+    
+    respond_to do |format|
+      format.html { redirect_to employees_url }
+      format.json { head :no_content }
+    end
   end
 
   private
